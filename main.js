86df11e917d1a026a89d61b2857ccbb54d73e99f
JHONATAN JANSSEN SEGURA GALINDO <jsegura-e@ripley.com.pe>
2022-09-01 16:10:32 -0500

    ADDED: ruta relativa en package.json(homepage) & electron

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    autoHideMenuBar: true,
+  });
+  if (process.env.DEBUG) win.webContents.openDevTools();
+  if (process.env.DEBUG) {
+    win.loadURL("http://localhost:3000");
+    console.log("DEVELOPMENT");
+  } else {
+    win.loadFile("./build/index.html");
+    console.log("TO PRODUCTION");
+  }
+};
+
+app.on("ready", createWindow);
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows.length === 0) createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.env.platform !== "darwin") app.quit();
+});
